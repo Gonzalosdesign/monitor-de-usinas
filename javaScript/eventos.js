b0b9efbb7dc8bd07d85fa6aa9e7172529991454c
Gonzalo Souto <gonzalosdesign@gmail.com>
2023-06-17 18:13:52 -0300

    puliendo detalles de estilo y mejorando visualición d datos

--- javaScript/eventos.js
+++ javaScript/eventos.js
@@ -18,24 +18,27 @@ function eventos(){
         //Condicional define img src segun valor de la propiedad del objeto
         item.motorStatus ? item.img = "./img/motorON.png" : item.img = "./img/motorOFF.png";
 
-        
-        
         //crea una card por cada objeto
         let card = document.createElement("div");
         card.className = "col-xs-12 col-sm-6 col-md-4";
         card.id = `card${item.id}`;
         card.innerHTML = `
         <div>
-        <div class="monitor rounded-2">
-        <h3>Terminal: ${item.id}</h3>
-        <img src="${item.img}">
-        <p>Nivel del tanque:</p>
-        <hr class="nivel" id="${item.nivel}" width="${item.nivel}%">
-        <button class="btn btn-primary" id="boton${item.id}">Ver más</button>
-        </div>
+          <div class="monitor rounded-2">
+            <h3>Terminal: ${item.id}</h3>
+            <div class="row">
+              <div class="mitad text-center">
+                <img src="${item.img}">
+              </div>
+              <div class="mitad">
+                <p>Nivel del tanque: ${item.nivel}%</p>
+                <hr class="nivel" id="${item.nivel}" width="${item.nivel}%">
+                <button class="btn btn-primary" id="boton${item.id}">Ver más</button>
+              </div>
+            </div>
+          </div>
         </div>
         `
-        
         //añade la card al DOM
         gallery.appendChild(card);
         //llama a la funcion verMas y le pasa el parametro
--- javaScript/verMas.js
+++ javaScript/verMas.js
@@ -10,10 +10,15 @@ function verMenos (id){
         <div>
           <div class="monitor rounded-2">
             <h3>Terminal: ${id}</h3>
-            <img src="${img}">
-            <p>Nivel del tanque:</p>
-            <hr class="nivel" id="${arreglo[id].nivel}" width="${arreglo[id].nivel}%">
-            <button class="btn btn-primary" id="boton${id}">Ver más</button>
+            <div class="row">
+            <div class="mitad text-center">
+                <img src="${img}">
+              </div>
+              <div class="mitad">
+                <p>Nivel del tanque: ${arreglo[id].nivel}%</p>
+                <hr class="nivel" id="${arreglo[id].nivel}" width="${arreglo[id].nivel}%">
+                <button class="btn btn-primary" id="boton${id}">Ver más</button>
+              </div>
           </div>
         </div>
         `
@@ -34,20 +39,26 @@ function verMas(id){
     card.innerHTML = `
           <div>
             <div class="monitor rounded-2">
-              <h3>Terminal: ${id}</h3>
-              <ul>
-                <li>Ubicación: ${arreglo[id].ubicacion}</li>
-                <li>Temperatura: ${arreglo[id].temp}º</li>
-                <li>Voltaje: ${arreglo[id].volt}V</li>
-                <li>Presión: ${arreglo[id].presion}Kg/cm2</li>
-                <li>Marca: ${arreglo[id].marca}</li>
-                <li>Modelo: ${arreglo[id].modelo}</li>
-                <li>Comentarios: ${arreglo[id].comment}</li>
-                <li>Nivel de tanque: ${arreglo[id].nivel}%</li>
-              </ul>
-              <img src="${img}">
-              <hr class="nivel" id="${arreglo[id].nivel}" width="${arreglo[id].nivel}%">
-              <button class="btn btn-primary" id="boton${id}">Ver menos</button>
+            <h3>Terminal: ${id}</h3>
+              <div class="row">
+              <div class="mitad text-center">
+                  <img src="${img}">
+                  <p>Nivel de tanque: ${arreglo[id].nivel}%</p>
+                  <hr class="nivel" id="${arreglo[id].nivel}" width="${arreglo[id].nivel}%">
+                  <button class="btn btn-primary" id="boton${id}">Ver menos</button>
+                </div>
+                <div class="mitad">
+                  <ul>
+                    <li>Ubicación: ${arreglo[id].ubicacion}</li>
+                    <li>Temperatura: ${arreglo[id].temp}º</li>
+                    <li>Voltaje: ${arreglo[id].volt}V</li>
+                    <li>Presión: ${arreglo[id].presion}Kg/cm2</li>
+                    <li>Marca: ${arreglo[id].marca}</li>
+                    <li>Modelo: ${arreglo[id].modelo}</li>
+                    <li>Comentarios: ${arreglo[id].comment}</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
           `
